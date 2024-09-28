3fec7cd470aab669fbdefdc5cd3d8e94b514e0ac
davsong01 <davsong16@gmail.com>
2024-09-28 15:27:38 +0100

    Verify actions

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -76,7 +76,7 @@ class CustomerController extends Controller
     function unverifiedCustomers(Request $request, $status = null)
     {
         set_time_limit(360);
-        $customers = User::whereNull('email_verified_at')->get();
+        $customers = User::whereNull('email_verified_at')->orderBy('created_at', 'DESC')->get();
         return view('admin.customers.unverified', ['customers' => $customers]);
     }
 
